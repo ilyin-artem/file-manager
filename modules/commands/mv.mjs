e90a358d66b6329f7d396f8f4fe0498c029bc057
Artyom Ilyin <ilyin@infostorm.ru>
2022-06-12 07:42:41 +0300

    add: mv command

---
+++ modules/commands/mv.mjs
@@ -0,0 +1,65 @@
+import { readdir, mkdir, unlink } from 'fs/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { join, isAbsolute, dirname, basename } from 'path';
+
+import { isDirectory } from '../isDirectory.mjs';
+import { checkFileExists } from '../checkFileExists.mjs';
+import { messageFailed, messageFileSuccess } from '../messages.mjs';
+
+export const mv = async (currentDir, fileSource, fileTarget) => {
+    if (!(await isAbsolute(fileSource)))
+        fileSource = join(currentDir, fileSource);
+
+    if (!(await isAbsolute(fileTarget)))
+        fileTarget = join(dirname(fileSource), fileTarget);
+
+    if (!(await checkFileExists(fileSource))) {
+        messageFailed();
+        return;
+    }
+
+    await moveFiles(fileSource, fileTarget);
+};
+
+const moveFiles = async (folderSource, folderTarget) => {
+    let sourceFile;
+    let targetFile;
+
+    try {
+        if (await isDirectory(folderSource)) {
+            if (!(await checkFileExists(folderTarget)))
+                await mkdir(folderTarget);
+            const data = await readdir(folderSource);
+
+            for (const file of data) {
+                sourceFile = join(folderSource, file);
+                targetFile = join(folderTarget, file);
+                if (await checkFileExists(targetFile)) throw 'File exists';
+                // copyFile(sourceFile, targetFile);
+
+                createReadStream(sourceFile).pipe(
+                    createWriteStream(targetFile)
+                );
+                await unlink(sourceFile);
+                messageFileSuccess('moved', folderSource);
+            }
+        } else {
+            // if file
+            if (await isDirectory(folderTarget)) {
+                targetFile = join(folderTarget, basename(folderSource));
+                // copyFile(folderSource, targetFile);
+                if (await checkFileExists(targetFile)) throw 'File exists';
+
+                await createReadStream(folderSource).pipe(
+                    createWriteStream(targetFile)
+                );
+                await unlink(folderSource);
+                messageFileSuccess('moved', folderSource);
+            } else {
+                messageFailed();
+            }
+        }
+    } catch (error) {
+        messageFailed();
+    }
+};
